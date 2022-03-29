a8b6d077032bfc2668e14c44e1f6b1dd47faa829
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2022-03-28 23:43:51 -0700

    Update app.js

--- JavaScript-clock-main/JavaScript-clock-main/app.js
+++ JavaScript-clock-main/JavaScript-clock-main/app.js
@@ -1,29 +1,23 @@
 function showTime(){
-    let date = new Date();
-    let h = date.getHours(); // 0 - 23
-    let m = date.getMinutes(); // 0 - 59
-    let s = date.getSeconds(); // 0 - 59
-    let session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    let time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
+let s,m,h,time,sesion ,date;
+date=new Date();
+s=date.getSeconds();
+m=date.getMinutes();
+h=date.getHours();
+s=(s<10)? "0" + s : s ;
+m=(m<10)? "0" + m : m ;
+h=(h<10)? "0" + h : h ;
+sesion =" AM";
+if(h>12){
+    h=h-12;
+    sesion="PM";
 }
-
+if(h==0){
+    h=12;
+}
+time= h + " : " + m + " : " + s + " " + sesion ;
+document.getElementById("time").innerHTML=time;
+document.getElementById("time").textContent=time;
+setTimeout(showTime, 1000);
+}
 showTime();
